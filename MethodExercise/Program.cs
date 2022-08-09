d9bdf07648e4fd194eb9a380f7315b2f63b98eda
Ashley Tucker <ashleyst35@aol.com>
2022-08-09 18:35:47 -0400

    exercise2

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");// output
+var name = Console.ReadLine(); // Input
+
+Console.WriteLine("What is your favorite cereal?");
+var cereal = Console.ReadLine();
+
+Console.WriteLine("What is your favorite donut?");
+var donut = Console.ReadLine();
+
+Console.WriteLine("What is your favorite Dog?");
+var dog = Console.ReadLine();
+
+Console.WriteLine($" Hi my name is {name}, my favorite is {cereal}, my favorite is {donut}, my favorite is {dog}");
+
+
+
+
+static int Add(int x, int y)
+{
+    return (x + y);
+
+
+}
+Console.WriteLine(Add(1, 5));
+
+
+static int Multiply(int num1, int num2)
+{
+    return (4 * 4);
+}
+Console.WriteLine(Multiply(4,4));
+
+
+static int Subtract(int num1, int num2)
+{
+    return (2 - 2);
+
+
+}
+    Console.WriteLine(Subtract(2,2));
+
+
+
+
+
+
+
+
+
+
+
